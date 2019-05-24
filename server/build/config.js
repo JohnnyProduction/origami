fb8732c9fd1503beafaeac9661906d1cd363f583
Павел Фанасков <pfanaskov@ivi.ru>
2019-05-24 15:33:51 +0300

    remove build

--- server/build/config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CONFIG = {
-    host: "localhost",
-    port: 8778,
-    sqlitePath: "./dev_db.sqlite",
-};
--- server/build/model/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const autor_1 = require("./autor");
-const category_1 = require("./category");
-// TODO сделать initialize и toPlain обязательным в моделях
-exports.initAllModels = (database) => {
-    autor_1.AutorModel.initialize(database);
-    category_1.CategoryModel.initialize(database);
-};
