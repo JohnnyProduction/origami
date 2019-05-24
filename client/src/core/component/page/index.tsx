e454cbf2a5b81e8ef6859579e55d76827caf7746
Павел Фанасков <pfanaskov@ivi.ru>
2019-05-24 20:53:01 +0300

    client base architecture wip

---
+++ client/src/core/component/page/index.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export class Page extends React.Component {
+    
+}
---
+++ client/src/pages/origamy/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Page } from "../../core/component/page";
+
+export class OrigamyPage extends React.Component {
+    public render() {
+        return (
+            <Page>
+                
+            </Page>
+        );
+    }
+}
